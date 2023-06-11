a77c72ab7cecd9d64b4a84efd4afc88bb7b741f0
aitorgomezUNIR <aitor.gomez166@comunidadunir.net>
2023-06-11 19:26:36 +0200

    Initial commit

---
+++ src/main/java/com/gestorreservas/GestorReservasApplication.java
@@ -0,0 +1,13 @@
+package com.gestorreservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorReservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestorreservas/GestorReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestorreservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
